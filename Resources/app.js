2cbe005a9232418c2c6516cb79aedab16da0f409
publicarray <s_mail_s@yahoo.com>
2013-09-25 21:22:19 +1000

    Revert "Merge branch 'Develop'"
    This reverts commit bab9f3028b6b628f13056b11dc55058033cd5759, reversing
    changes made to 6e3ece18b021ee16739fdd3814c7aef003237d75.

--- Resources/app.js
+++ Resources/app.js
@@ -5,7 +5,7 @@
  * http://developer.appcelerator.com/question/122327/save-and-reload-state-from-edited-tab-view
  */
 
-Ti.include('appModel.js');
+Ti.include('macRumorsModel.js');
 //load in Window class
 var Window = require('windowFunction');
 
@@ -119,6 +119,45 @@ var tab6 = Ti.UI.createTab(
     window: settingsWin
 });
 
-Ti.include('appController.js');
+// Save Tab order on Pause/Exit --> when running in the background
+tabGroup.addEventListener('focus', function(e){
+    var currentTabId = e.index;
+    var k = [];
+    for (i = 0; i < 7; i++)
+    {
+        if(tabGroup.tabs[i].id != undefined){
+            var tabId = tabGroup.tabs[i].id;
+            k.push(tabId);
+        }
+    }
+    //Save properties -> the last tab opened and the tab order list.
+    Ti.App.Properties.setInt('lastTB', currentTabId);
+    Ti.App.Properties.setList('TB', k);
+    
+    // for debuging
+    //Ti.API.info(Ti.App.Properties.getList('TB'));
+    //Ti.API.info(Ti.App.Properties.getInt('lastTB', 0));
+});
+
+// Load Tabs back in Order
+for (i = 0; i < 7; i++)
+{
+    // Try to get saved values for Tab Order
+    var k = Ti.App.Properties.getList('TB');
+    if (k === null)
+    {
+        // If don't have saved, populate in Designer order
+        var temp = eval('tab' + [i]);
+        tabGroup.addTab(temp);
+    }
+    else
+    {
+        var temp = eval('tab' + parseInt(k[i]));
+        // if have saves, populate
+        tabGroup.addTab(temp);
+    }
+}
+//open last opened tab from property
+tabGroup.setActiveTab(Ti.App.Properties.getInt('lastTB', 0));
 // open tab Group
 tabGroup.open();
--- Resources/rssController.js
+++ Resources/rssController.js
@@ -86,7 +86,6 @@ function showTable() {
     tintColor :Ti.App.Properties.getString('theme', '#980012'),
     height:43,
     top:0,
-    showCancel:true,
     });
     // create table
     var tableView = Ti.UI.createTableView({
@@ -123,65 +122,15 @@ function showTable() {
         //Hide the activity indicator when the funtion has completed
         activityIndicator.hide();
     });
-    //save search when user types
     searchBar.addEventListener('change', function (e) {
-        Ti.App.Properties.setString('search', e.value);
+        Ti.App.Properties.setString('search', searchBar.value);
     });
-    //delete saved value on cancel
-    searchBar.addEventListener('cancel', function (e) {
-        Ti.App.Properties.setString('search', null);
-    });
-    // realoads entered search value & updates table <-- IT'S BUGGY!!!
-    // http://developer.appcelerator.com/apidoc/mobile/1.8.2/Titanium.UI.SearchBar-object
-    win.addEventListener('focus', function(e) {;
-        searchBar.value=Ti.App.Properties.getString('search','');
-        //searchBar.fireEvent('focus');
-        if(searchBar.value){
-            //searchBar.focus();
-            searchBar.blur();
-            tableView.searchHidden=false;
-        } 
-    });
-    /* debuging - bugs sitll presists
-     
-     
-    searchBar.addEventListener('focus', function (e) {
-        Ti.API.info('search is in focus with value:' + e.value);
-    });
-    searchBar.addEventListener('change', function (e) {
-        Ti.API.info('search has changed with value:' + e.value);
-    });
-    searchBar.addEventListener('return', function (e) {
-        Ti.API.info('search is return with value of:' + e.value);
-    });
-    // redisplay the value saved when user returns to the tab - issue is that the table isn't uptated correctly in ios 7
     win.addEventListener('focus', function(e) {
-       // searchBar.focus();
-        //searchBar.show();
-        //searchBar.hide();
-        searchBar.value=Ti.App.Properties.getString('search','');
-        //searchBar.fireEvent('change');
-        //searchBar.setValue(Ti.App.Properties.getString('search',''));
-        //searchBar.blur();
-        //searchBar.show();
-        //searchBar.hide();
-       // searchBar.blur();
-        //searchBar.hide();
-        //searchBar.fireEvent('change', {e:"Ti.App.Properties.getString('search','')"});
-        searchBar.fireEvent('focus');
-        //searchBar.fireEvent('change');
-        //searchBar.fireEvent('return');
         if(searchBar.value){
-            searchBar.focus();
-            searchBar.blur();
-            //tableView.fireEvent('click');
-            tableView.searchHidden=false;
-            //searchBar.show();
-            //searchBar.fireEvent('return');
+        searchBar.value=Ti.App.Properties.getString('search','');
+        tableView.searchHidden=false; 
         }
-        
     });
-    */
 };
 
 function getXMLdata(file) {
